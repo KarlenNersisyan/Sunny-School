e4c155f2be2f62c4304923ef2183e83269bf5773
Karlen Nersisyan <karlennersisyan814@gmail.com>
2024-04-06 19:00:00 +0400

    Theme Color

--- todo-list/src/app/App.tsx
+++ todo-list/src/app/App.tsx
@@ -1,17 +1,81 @@
+import React, { Suspense, useEffect, useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+
 //* Components
+import AboutPageAsync from '@/pages/AboutPage/AboutPage.async';
+import MainPageAsync from '@/pages/MainPage/MainPage.async';
 import { Button } from '@/components/ui/button';
 
-//* Style
-import styles from './app.module.css';
+const App: React.FC = () => {
+	const [theme, setTheme] = useState<'light' | 'dark'>('light');
 
-const App = () => {
+	useEffect(() => {
+		const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+		const handleChange = (e: MediaQueryListEvent) => {
+			setTheme(e.matches ? 'dark' : 'light');
+		};
+
+		darkModeMediaQuery.addEventListener('change', handleChange);
+
+		setTheme(darkModeMediaQuery.matches ? 'dark' : 'light');
+
+		return () => {
+			darkModeMediaQuery.removeEventListener('change', handleChange);
+		};
+	}, []);
+
+	useEffect(() => {
+		document.body.classList.remove('light', 'dark');
+		document.body.classList.add(theme);
+	}, [theme]);
+
+	const toggleTheme = () => {
+		setTheme(theme === 'light' ? 'dark' : 'light');
+	};
+
 	return (
-		<div className={styles.flex_center}>
-			<span className='text-3xl font-bold underline text-red-500'>Hello World!</span>
-			<span className={`${styles.name_block} text-2xl font-medium underline text-red-500`}> USER </span>
+		<div className={`flex flex-col h-screen`}>
+			<nav className='bg-slate-200'>
+				<div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+					<div className='flex items-center justify-between h-16'>
+						<div className='flex'>
+							<Link
+								to='/'
+								className='text-zinc-600 px-3 py-2 rounded-md text-md font-semibold'>
+								Main
+							</Link>
+							<Link
+								to='/about'
+								className='text-zinc-600 px-3 py-2 rounded-md text-md font-semibold'>
+								About
+							</Link>
+						</div>
+					</div>
+				</div>
+			</nav>
 
 			<div>
-				<Button>Click me</Button>
+				<Button
+					onClick={toggleTheme}
+					className='absolute top-0 right-0 m-5 p-2'>
+					Change them mode
+				</Button>
+			</div>
+
+			<div className='flex-grow p-5'>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route
+							path='/'
+							element={<MainPageAsync />}
+						/>
+						<Route
+							path='/about'
+							element={<AboutPageAsync />}
+						/>
+					</Routes>
+				</Suspense>
 			</div>
 		</div>
 	);
--- todo-list/src/main.tsx
+++ todo-list/src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 //* Style
 import './index.css';
 
 //* Components
-import App from './app/App.tsx';
+import App from './app/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
